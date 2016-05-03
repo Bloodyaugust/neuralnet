43437e42be7ddf9b9baa8d6905ee30175d6a16d7
Greyson <greysonrichey@gmail.com>
2016-05-02 18:10:42 -0600

    More basic-ass shit.

--- app/index.js
+++ app/index.js
@@ -3,7 +3,10 @@ $(function () {
     $('body').addClass('loaded');
 
     setTimeout(function () {
+      var $canvas = $('canvas');
+
       $('header').addClass('show');
+      $('canvas').addClass('show');
     }, 1000);
   }, 500);
 });
